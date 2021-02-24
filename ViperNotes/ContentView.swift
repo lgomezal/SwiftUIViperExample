4262fd1921bcc5edaf19ba1f172d76a75bafc301
Luis Gomez Alonso <lgomezal@gmail.com>
2021-02-24 14:45:50 +0100

    Initial Commit

---
+++ ViperNotes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ViperNotes
+//
+//  Created by Luis Gómez Alonso on 24/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
